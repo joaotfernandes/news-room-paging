985332ff27a8975c63c01c43427635dc4cc40bed
Joao <joao.fernandes@sky.uk>
2018-08-27 19:48:59 +0100

    Configure dependencies with Koin

---
+++ app/src/main/java/com/github/joaotfernandes/newsroompaging/NewsApplication.kt
@@ -0,0 +1,33 @@
+package com.github.joaotfernandes.newsroompaging
+
+import android.app.Application
+import androidx.room.Room
+import com.github.joaotfernandes.newsroompaging.db.DATABASE_NAME
+import com.github.joaotfernandes.newsroompaging.db.NewsDb
+import com.github.joaotfernandes.newsroompaging.service.NewsService
+import com.github.joaotfernandes.newsroompaging.service.RetrofitNewsService
+import org.koin.android.ext.android.startKoin
+import org.koin.androidx.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+import java.util.Locale
+
+class NewsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val module = module {
+            single { RetrofitNewsService(BuildConfig.NEWS_API_KEY) as NewsService }
+
+            single {
+                Room.databaseBuilder(this@NewsApplication, NewsDb::class.java, DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+
+            viewModel { NewsViewModel(get(), get(), getProperty("article_keywords"), Locale.getDefault().language) }
+        }
+
+        startKoin(this, listOf(module))
+    }
+}
