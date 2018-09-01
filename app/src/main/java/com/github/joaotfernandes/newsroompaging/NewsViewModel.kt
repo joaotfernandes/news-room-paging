89494ca88889b228de53d4e092feca614d388c4f
Joao <joao.fernandes@sky.uk>
2018-09-01 16:25:16 +0100

    Add swipe to refresh functionality to articles

--- app/src/main/java/com/github/joaotfernandes/newsroompaging/NewsViewModel.kt
+++ app/src/main/java/com/github/joaotfernandes/newsroompaging/NewsViewModel.kt
@@ -3,8 +3,12 @@ package com.github.joaotfernandes.newsroompaging
 import androidx.lifecycle.ViewModel
 import com.github.joaotfernandes.newsroompaging.repository.NewsRepository
 
-class NewsViewModel(newsRepository: NewsRepository, keyword: String, language: String) : ViewModel() {
+class NewsViewModel(private val newsRepository: NewsRepository, private val keyword: String, private val language: String) : ViewModel() {
 
     val articles = newsRepository.getArticles(keyword, language)
+
+    val refreshState = newsRepository.onRefreshStateChange()
+
+    fun refresh() = newsRepository.requestRefresh(keyword, language)
 }
 
--- app/src/main/java/com/github/joaotfernandes/newsroompaging/repository/DbNewsRepository.kt
+++ app/src/main/java/com/github/joaotfernandes/newsroompaging/repository/DbNewsRepository.kt
@@ -1,16 +1,24 @@
 package com.github.joaotfernandes.newsroompaging.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.github.joaotfernandes.newsroompaging.db.NewsDb
 import com.github.joaotfernandes.newsroompaging.paging.ArticlesBoundaryCallback
 import com.github.joaotfernandes.newsroompaging.service.NewsService
 import com.github.joaotfernandes.newsroompaging.service.model.Article
+import java.util.concurrent.ExecutorService
 
 private const val PAGE_SIZE = 20
 
-class DbNewsRepository(private val newsDb: NewsDb, private val newsService: NewsService) : NewsRepository {
+class DbNewsRepository(
+    private val newsDb: NewsDb,
+    private val newsService: NewsService,
+    private val ioExecutor: ExecutorService
+) : NewsRepository {
+
+    private val refreshState = MutableLiveData<NewsRepository.RefreshState>()
 
     private val pagedListConfig = PagedList.Config.Builder()
         .setPageSize(PAGE_SIZE)
@@ -22,4 +30,29 @@ class DbNewsRepository(private val newsDb: NewsDb, private val newsService: News
             .setBoundaryCallback(ArticlesBoundaryCallback(newsDb, newsService, keyword, language, pagedListConfig.pageSize))
             .build()
     }
+
+    override fun requestRefresh(keyword: String, language: String) {
+        if (refreshState != NewsRepository.RefreshState.REFRESHING) {
+            refreshState.value = NewsRepository.RefreshState.REFRESHING
+
+            newsService.getArticles(keyword, language, null, null, pageSize = PAGE_SIZE)
+                .doOnError { refreshState.value = NewsRepository.RefreshState.COMPLETE }
+                .doOnSuccess {
+                    if (it.isSuccessful) {
+                        it.body()?.let { newsResponse ->
+                            ioExecutor.submit {
+                                newsDb.runInTransaction {
+                                    val articleDao = newsDb.articles()
+                                    articleDao.deleteAll()
+                                    articleDao.insertArticles(newsResponse.articles)
+                                }
+                                refreshState.postValue(NewsRepository.RefreshState.COMPLETE)
+                            }
+                        }
+                    }
+                }.run()
+        }
+    }
+
+    override fun onRefreshStateChange() = refreshState
 }
