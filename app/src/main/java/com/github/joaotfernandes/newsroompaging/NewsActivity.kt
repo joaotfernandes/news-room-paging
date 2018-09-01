a22c0ce2571dea2212075d6efd46057e0f469303
Joao <joao.fernandes@sky.uk>
2018-09-01 16:51:10 +0100

    Open article on click

--- app/src/main/java/com/github/joaotfernandes/newsroompaging/NewsActivity.kt
+++ app/src/main/java/com/github/joaotfernandes/newsroompaging/NewsActivity.kt
@@ -1,11 +1,14 @@
 package com.github.joaotfernandes.newsroompaging
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.joaotfernandes.newsroompaging.repository.NewsRepository
+import com.github.joaotfernandes.newsroompaging.service.model.Article
 import kotlinx.android.synthetic.main.activity_news.articlesRecyclerView
 import kotlinx.android.synthetic.main.activity_news.swipeRefreshLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -18,7 +21,7 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        val adapter = ArticlesAdapter()
+        val adapter = ArticlesAdapter(this::onArticleClicked)
         articlesRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         articlesRecyclerView.adapter = adapter
 
@@ -28,5 +31,11 @@ class NewsActivity : AppCompatActivity() {
         viewModel.refreshState.observe(this, Observer {
             swipeRefreshLayout.isRefreshing = it == NewsRepository.RefreshState.REFRESHING
         })
+    }
+
+    private fun onArticleClicked(article: Article) {
+        Intent(Intent.ACTION_VIEW)
+            .apply { data = Uri.parse(article.url) }
+            .also { startActivity(it) }
     }
 }
